fad4e2b18adba6167d6cfba953dc69a6b80605f9
Laura <laura.gooon@gmail.com>
2017-07-05 23:20:31 -0400

    initial

---
+++ popup.js
@@ -0,0 +1,24 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//   var checkPageButton = document.getElementById('checkPage');
+//   checkPageButton.addEventListener('click', function() {
+//
+//     chrome.tabs.getSelected(null, function(tab) {
+//       d = document;
+//
+//       var f = d.createElement('form');
+//       f.action = '#';
+//       f.method = 'post';
+//       var i = d.createElement('input');
+//       i.type = 'hidden';
+//       i.name = 'url';
+//       i.value = tab.url;
+//       f.appendChild(i);
+//       d.body.appendChild(f);
+//       f.submit();
+//     });
+//   }, false);
+// }, false);
+
+document.getElementById('takePic').onclick = function() {
+  var tabPic = chrome.tabs.captureVisibleTab();
+}
